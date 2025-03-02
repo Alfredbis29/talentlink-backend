2163c093315962beda7a912dfaea998fdfe21db1
Alfredbis29 <kalumunabisimwa5@gmail.com>
2025-03-02 17:01:39 +0200

    make the  job entity

--- src/modules/jobs/entities/job.entity.ts
+++ src/modules/jobs/entities/job.entity.ts
@@ -1,5 +1,12 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 import { User } from '../../users/entities/user.entity';
+
+export enum JobStatus {
+  OPEN = 'OPEN',
+  IN_PROGRESS = 'IN_PROGRESS',
+  COMPLETED = 'COMPLETED',
+  CANCELLED = 'CANCELLED'
+}
 
 @Entity('jobs')
 export class Job {
@@ -9,9 +16,28 @@ export class Job {
   @Column()
   title: string;
 
+  @Column('text')
+  description: string;
+
+  @Column('decimal', { precision: 10, scale: 2 })
+  budget: number;
+
+  @Column({
+    type: 'enum',
+    enum: JobStatus,
+    default: JobStatus.OPEN
+  })
+  status: JobStatus;
+
   @ManyToOne(() => User, user => user.postedJobs)
   client: User;
 
-  @ManyToOne(() => User, user => user.assignedJobs)
+  @ManyToOne(() => User, user => user.assignedJobs, { nullable: true })
   freelancer: User;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
 } 
